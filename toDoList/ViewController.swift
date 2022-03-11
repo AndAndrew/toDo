b15f4de3c4225b49fb4cac1ad4f24da67393c927
Andrew <andrew.krivokhizhin@gmail.com>
2022-03-11 15:24:49 +0400

    feat: add tableView on storyboard

--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.backgroundColor = .cyan
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "Hello!"
+        
+        return cell
+    }
+    
+    
+}
