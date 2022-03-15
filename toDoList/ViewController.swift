64f97f279ca72414d26e969d67dff7bce23b14bf
Andrew <andrew.krivokhizhin@gmail.com>
2022-03-15 14:41:49 +0400

    feat: added usage toDo array to save data

--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -20,8 +20,13 @@ class ViewController: UIViewController {
     @IBAction func addButtonDidTapped(_ sender: Any) {
         let alertController = UIAlertController(title: "Add what to do.", message: nil, preferredStyle: .alert)
         alertController.addTextField(configurationHandler: nil)
-        let okAction = UIAlertAction(title: "OK", style: .default) { UIAlertAction in
-            
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+            guard var toDoText = alertController.textFields![0].text else { return }
+            toDoText = toDoText.trimmingCharacters(in: .whitespaces)
+            if toDoText != "" {
+                self.toDo.insert(toDoText, at: 0)
+                self.tableView.reloadData()
+            }
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alertController.view.tintColor = .darkGray
@@ -34,14 +39,14 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        15
+        toDo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .clear
-        
+        cell.textLabel?.text = toDo[indexPath.row]
         return cell
     }
 }
